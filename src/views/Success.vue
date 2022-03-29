7711dd7a0334943a277ba6b25c2c37bb7b4f6df1
ElaineUX <elaine@seizethefire.com>
2022-03-29 12:16:22 +0200

    Contact Form again

--- src/views/Success.vue
+++ src/views/Success.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1>Oops, looks like something went wrong!</h1>
-    <router-link to="/">
+    <h1>You're Awesome! Thank you for your submission!</h1>
+    <router-link to="/home">
       <button>Back to form</button>
     </router-link>
   </div>
@@ -9,6 +9,6 @@
 
 <script>
   export default {
-    name: "SubmissionFail"
+    name: "success"
   }
-</script>
+</script> 
