5c2e8f1d4efd69c8f389373b7fa95beb6cbbbd1d
Devin <devin@blackhat.co.za>
2022-03-30 12:24:30 +0200

    Update Contact.vue

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -82,64 +82,7 @@
 
           <!-- Contact form -->
 
-          <div class="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
-            <h3 class="text-lg font-medium text-gray-900">Send us a message</h3>
-            <form method="POST" action="success" name="contact_us" class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8" data-netlify="true">
-              <input type="hidden" name="form-name" value="contact_us" />
-              <p className="hidden">
-                  <label>Don't fill this out if you're human: <input name="bot-field" /></label>
-              </p>
-              <div>
-                <label for="first-name" class="block text-sm font-medium text-gray-900">First name</label>
-                <div class="mt-1">
-                  <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 rounded-md contact-form-border" />
-                </div>
-              </div>
-              <div>
-                <label for="last-name" class="block text-sm font-medium text-gray-900">Last name</label>
-                <div class="mt-1">
-                  <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 rounded-md contact-form-border" />
-                </div>
-              </div>
-              <div>
-                <label for="email" class="block text-sm font-medium text-gray-900">Email</label>
-                <div class="mt-1">
-                  <input id="email" name="email" type="email" autocomplete="email" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 rounded-md contact-form-border" />
-                </div>
-              </div>
-              <div>
-                <div class="flex justify-between">
-                  <label for="phone" class="block text-sm font-medium text-gray-900">Phone</label>
-                  <span id="phone-optional" class="text-sm text-gray-500">Optional</span>
-                </div>
-                <div class="mt-1">
-                  <input type="text" name="phone" id="phone" autocomplete="tel" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 rounded-md contact-form-border" aria-describedby="phone-optional" />
-                </div>
-              </div>
-              <div class="sm:col-span-2">
-                <label for="subject" class="block text-sm font-medium text-gray-900">Subject</label>
-                <div class="mt-1">
-                  <input type="text" name="subject" id="subject" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 rounded-md contact-form-border" />
-                </div>
-              </div>
-              <div class="sm:col-span-2">
-                <div class="flex justify-between">
-                  <label for="message" class="block text-sm font-medium text-gray-900">Message</label>
-                  <span id="message-max" class="text-sm text-gray-500">Max. 500 characters</span>
-                </div>
-                <div class="mt-1">
-                  <textarea id="message" name="message" rows="4" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border border-gray rounded-md" aria-describedby="message-max" />
-                </div>
-              </div>
-              <div class="sm:col-span-2 sm:flex sm:justify-end">
-                <button type="submit" class="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto button-main">Submit</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+
 
 TEST
 <form name="contact2" method="POST" enctype="application/x-www-form-urlencoded" data-netlify="true" netlify>
