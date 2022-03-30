2d6650d2cdcaca320353ef244c29af72771de0f5
Devin <devin@blackhat.co.za>
2022-03-30 11:26:05 +0200

    forms test

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -142,7 +142,7 @@
   </div>
 
 TEST
-<form name="contact2" method="POST" data-netlify="true">
+<form name="contact2" method="POST" data-netlify="true" netlify>
   <p>
     <label>Your Name: <input type="text" name="name" /></label>
   </p>
