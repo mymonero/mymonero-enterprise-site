ccb4ecd57a7f4bb089848ca70eca65f4878c14dc
ElaineUX <elaine@seizethefire.com>
2022-06-15 12:51:22 +0200

    Added New Partner Testimonial

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -9,14 +9,23 @@
        <p class="mt-3 max-w-3xl mx-auto text-xl text-gray-500 sm:mt-4">Powering the Monero functionality for some of the worlds top crypto wallets.<br>Some of our partners:<br><br></p>
     </div>
     </div>
+  </section>
+  <br><br><br><br><br><br><br>
 
-    <div class="max-w-7xl mx-auto md:grid md:grid-cols-2 md:px-6 lg:px-8 bg-lightsilver testimonial-border">
-      <div class="py-12 px-4 sm:px-6 md:flex md:flex-col md:py-16 md:pl-0 md:pr-10  lg:pr-16">
-        <div class="md:flex-shrink-0">
-          <img class="h-12" src="/edge-logo.svg" alt="Tuple" />
-        </div>
-        <blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
-          <div class="relative text-md font-medium md:flex-grow text-silver">
+
+<div class="bg-white"> 
+    <section class="-mt-32 max-w-7xl mx-auto relative z-10 pb-32 px-4 sm:px-6 lg:px-8" aria-labelledby="contact-heading">
+      <div class="grid grid-cols-1 gap-y-20 lg:grid-cols-3 lg:gap-y-20 lg:gap-x-8">
+
+<!-- Edge -->
+        <div class="flex flex-col bg-lightsilver rounded-2xl shadow-xl ">
+          <div class="flex-2 relative pt-16 px-6 pb-8 md:px-8 ">
+            <div class="absolute top-0 p-5 inline-block bg-indigo-600 rounded-xl shadow-lg transform -translate-y-1/2 ">
+              <div class="md:flex-shrink-0">
+                <img class="h-12" src="/edge-logo.svg" alt="Tuple" />
+              </div>
+            </div>
+            <div class="relative text-md font-medium md:flex-grow text-silver">
             <svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 testimonial-text-quote" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
               <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
             </svg>
@@ -31,20 +40,23 @@
               </div>
             </div>
           </footer>
-        </blockquote>
-      </div>
-
-      <div class="py-12 px-4 border-t-2 sm:px-6 md:py-16 md:pr-0 md:pl-10 md:border-t-0 md:border-l lg:pl-16 testimonial-line">
-        <div class="md:flex-shrink-0">
-          <img class="h-12" src="/zelcore-logo.svg" alt="Workcation" />
+          </div>
         </div>
-        <blockquote class="mt-6 md:flex-grow md:flex md:flex-col">
-          <div class="relative text-md font-medium md:flex-grow text-silver">
-            <svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 testimonial-text-quote" fill="currentColor" viewBox="0 0 32 32">
+
+<!-- Zelcore -->
+<div class="flex flex-col bg-lightsilver rounded-2xl shadow-xl ">
+          <div class="flex-2 relative pt-16 px-6 pb-8 md:px-8 ">
+            <div class="absolute top-0 p-5 inline-block bg-indigo-600 rounded-xl shadow-lg transform -translate-y-1/2">
+              <div class="md:flex-shrink-0">
+                <img class="h-12" src="/zelcore-logo.svg" alt="Workcation" />
+              </div>
+            </div>
+            <div class="relative text-md font-medium md:flex-grow text-silver">
+            <svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 testimonial-text-quote" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
               <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
             </svg>
             <p class="relative">“In our quest to add the best-in-class support in Zelcore, we wanted to work with the best Monero support solution, and that is MyMonero! <br><br>
-                They have built a backend that allows us to push amazing service and support to our customers.”</p>
+                They have built a backend that allows us to push amazing service and support to our customers.”</p><br><br>
           </div>
           <footer class="mt-8">
             <div class="flex items-start testimonial-ceo">
@@ -54,12 +66,37 @@
               </div>
             </div>
           </footer>
-        </blockquote>
+          </div>
+        </div>
+
+<!-- Bitrequest -->
+<div class="flex flex-col bg-lightsilver rounded-2xl shadow-xl ">
+          <div class="flex-2 relative pt-16 px-6 pb-8 md:px-8 ">
+            <div class="absolute top-0 p-5 inline-block bg-indigo-600 rounded-xl shadow-lg transform -translate-y-1/2">
+              <div class="md:flex-shrink-0">
+                <img class="h-12" src="/bitrequest_logo.svg" alt="Workcation" />
+              </div>
+            </div>
+            <div class="relative text-md font-medium md:flex-grow text-silver">
+            <svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 testimonial-text-quote" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
+              <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+            </svg>
+            <p class="relative">“Thanks to MyMonero Enterprise Services we managed to get full Monero support for Bitrequest! <br><br>
+                  We are happy that we can provide Monero support in our Point-of-Sale and Payment-Request system. We are also excited to have the full support of MyMonero's infrastructure and Team.”</p><br>
+          </div>
+          <footer class="mt-8">
+            <div class="flex items-start testimonial-ceo">
+              <div class="ml-4">
+                <div class="text-base font-medium text-silver">- Martijn Kremers</div>
+                <div class="text-base font-medium text-silver">&nbsp;&nbsp;Bitrequest.io</div>
+              </div>
+            </div>
+          </footer>
+          </div>
+        </div>
+
       </div>
-
-    </div>
-  </section>
-
-<br>
+    </section>
+  </div>
 
 </template>
