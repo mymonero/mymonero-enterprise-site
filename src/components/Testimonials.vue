afc0b5a5e5a7392d77b5c57bac656f6adff94d12
ElaineUX <elaine@seizethefire.com>
2023-04-04 13:26:16 +0200

    Added Testamony

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -95,6 +95,32 @@
           </div>
         </div>
 
+        <!-- Atomic Wallet -->
+<div class="flex flex-col bg-lightsilver rounded-2xl shadow-xl ">
+          <div class="flex-2 relative pt-16 px-6 pb-8 md:px-8 ">
+            <div class="absolute top-0 p-5 inline-block bg-indigo-600 rounded-xl shadow-lg transform -translate-y-1/2">
+              <div class="md:flex-shrink-0">
+                <img class="h-12" src="/atomic-logo.svg" alt="Atomic" />
+              </div>
+            </div>
+            <div class="relative text-md font-medium md:flex-grow text-silver">
+            <svg class="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 testimonial-text-quote" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
+              <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+            </svg>
+            <p class="relative">“Thanks to MyMonero we are able to provide Monero support on Atomic Wallet!<br><br>
+              Since integration, everything has been running smoothly and their support is top of the class, always available and ready to assist.”</p><br>
+          </div>
+          <footer class="mt-8">
+            <div class="flex items-start testimonial-ceo">
+              <div class="ml-4">
+                <div class="text-base font-medium text-silver">- Konstantin Gladych</div>
+                <div class="text-base font-medium text-silver">&nbsp;&nbsp;CEO</div>
+              </div>
+            </div>
+          </footer>
+          </div>
+        </div>
+
       </div>
     </section>
   </div>
